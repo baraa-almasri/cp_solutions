5f6429d2dc8d13f863717515ac9c9cab7242388a
Baraa Al-Masri <hexagon16rpm@yandex.com>
2020-08-06 13:43:23 +0300

    Add files via upload

--- codeforces/91 A.Chips.cpp
+++ codeforces/91 A.Chips.cpp
@@ -1,13 +1,28 @@
-// problem link
+// https://codeforces.com/contest/92/problem/A
 #include <stdio.h>
-#include <math.h>
 
 #define forK(start, end) for(int k = start; k < end; k++)
 #define forL(start, end) for(int l = start; l < end; l++)
 typedef long long int lli;
  
 int main() {
+    int n;
+    int m;
+    scanf("%d %d", &n, &m);
 
+    forK(1, n + 1) {
+        if( m < k) {
+            break;
+        }
+
+        m -= k;
+        if(k == n) {
+            k = 0;
+            continue;
+        }
+        
+    }    
+    printf("%d\n", m);
 
     return 0;
 }

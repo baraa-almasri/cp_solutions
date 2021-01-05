52754562aeff4e4d0aa22fe7cdfa170390866e87
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-01-05 22:20:26 +0200

    hmm

---
+++ codeforces/387-B.George and Round.cpp
@@ -0,0 +1,76 @@
+// https://codeforces.com/contest/387/problem/B
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <functional>
+#include <deque>
+
+#define var auto
+#define forI(start, end) for(int i = start; i < end; i++)
+#define forJ(start, end) for(int j = start; j < end; j++)
+#define forK(start, end) for(int k = start; k < end; k++)
+#define forL(start, end) for(lli l = start; l < end; l++)
+typedef long long lli;
+#define all(v) v.begin(), v.end()
+using namespace std;
+
+//#define DEBUG_BLYAT
+#ifdef DEBUG_BLYAT
+template <typename type>
+std::ostream& operator<<(std::ostream& os, std::vector<type> vec){
+    os << "[";
+    for (int i = 0; i < vec.size(); ++i) { 
+        os << vec[i]; 
+        os << (i != vec.size() - 1? ",": ""); 
+    }
+    os << "]";
+    return os;
+}
+
+inline bool charExist(char chr, std::string str) {  
+    return str.find(chr) != std::string::npos;
+}
+
+inline bool isPrime(lli n) {
+    forK(2, n/2) 
+		if(n % k == 0)
+            return false;
+
+    return true;
+}
+#endif
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+    // start of shit blyat
+
+    int n, m;
+    scanf("%d %d", &n , &m);
+    vector<int> req(n);
+    for_each(all(req), [](int &a){scanf("%d", &a);});
+    vector<int> prep(m);
+    for_each(all(prep), [](int &a){scanf("%d", &a);});
+
+    var reqIndex{0};
+    var prepIndex{0};
+
+    while (reqIndex < n && prepIndex < m) {
+        if (req[reqIndex] <= prep[prepIndex]) {
+            reqIndex++;
+        }
+        prepIndex++;
+    }
+
+    cout << n - reqIndex << endl;
+
+    return 0;
+}

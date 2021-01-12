e29769193f4721bf02c97fc1a250415ace038c37
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-01-12 03:57:55 +0200

    hmm

---
+++ codeforces/214-A.System of Equations.cpp
@@ -0,0 +1,68 @@
+// https://codeforces.com/contest/214/problem/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <functional>
+
+#define var auto
+#define forI(start, end) for(int i = start; i < end; i++)
+#define forJ(start, end) for(int j = start; j < end; j++)
+#define forK(start, end) for(int k = start; k < end; k++)
+#define forL(start, end) for(lli l = start; l < end; l++)
+typedef long long lli;
+#define all(v) v.begin(), v.end()
+using namespace std;
+
+//#define DEBUG_BLYAT
+#ifdef DEBUG_BLYAT
+template <typename type>
+std::ostream& operator<<(std::ostream& os, std::vector<type> vec){
+    os << "[";
+    for (int i = 0; i < vec.size(); ++i) { 
+        os << vec[i]; 
+        os << (i != vec.size() - 1? ",": ""); 
+    }
+    os << "]";
+    return os;
+}
+
+inline bool charExist(char chr, std::string str) {  
+    return str.find(chr) != std::string::npos;
+}
+
+inline bool isPrime(lli n) {
+    forK(2, n/2) 
+		if(n % k == 0)
+            return false;
+
+    return true;
+}
+
+#endif
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+    // start of shit blyat
+    int n, m;
+    scanf("%d %d", &n, &m);
+    var possiblePairs{0};
+
+    forI(0, n+1) {
+        forJ(0, m+1) {
+            possiblePairs += ( ((i*i) + j) == n ) && ( (i + (j*j)) == m );
+        }
+    }
+
+    printf("%d\n", possiblePairs);
+
+    return 0;
+}

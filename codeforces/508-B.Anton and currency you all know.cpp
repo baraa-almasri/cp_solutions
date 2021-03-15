e0306ea0d079e2592a5c85f5212912dfb5d04df4
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-03-15 21:52:00 +0200

    46ms after 7 failed submissions!

---
+++ codeforces/508-B.Anton and currency you all know.cpp
@@ -0,0 +1,72 @@
+// https://codeforces.com/problemset/problem/508/B
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void swap(char* c1, char* c2) {
+    char tmp = *c1;
+    *c1 = *c2;
+    *c2 = tmp;
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string num;
+    cin >> num;
+
+    int size{num.length()};
+    int i{0}, rightEven{size-1};
+
+    while (i < size) {
+        if (num[i] % 2 == 0) {
+            rightEven = i;
+        }
+        if (num[i]+1 <= num.back() && num[i] % 2 == 0) {
+            break;
+        }
+        i++;
+    }
+
+    bool foundEven{num[rightEven] % 2 == 0};    
+    swap(
+        &(i != size? num[i]: num[rightEven]), 
+        &num.back()
+    );
+
+    cout << (foundEven? num: "-1") << '\n';    
+
+    return 0;
+}

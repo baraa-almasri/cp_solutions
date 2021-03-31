b5018e35599c8f4f6d8810ca7c635fa5160786d8
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-04-01 00:00:35 +0300

    a blast from the past!

---
+++ codeforces/750-A.New Year and Hurry.cpp
@@ -0,0 +1,63 @@
+// https://codeforces.com/problemset/problem/750/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int problems{0};
+    for (int i = 1; i <= n; i++) {
+        if (k + 5*i <= 240) {
+            problems++;
+            k += 5*i;
+        }
+    }
+
+    printf("%d\n", problems);
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

748fe532dba296d17076ab771bf7465c3f160073
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-05-16 01:24:43 +0300

    woah

---
+++ codeforces/272-A.Dima and Friends.cpp
@@ -0,0 +1,67 @@
+// https://codeforces.com/problemset/problem/272/A 
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    int n;
+    scanf("%d", &n);
+    int m, fingers{0};
+    for (int i{0}; i < n; i++) {
+        scanf("%d", &m);
+        fingers += m;
+    }
+
+    int fingers2Show{0};
+    for (int i{1}; i <= 5; i++) {
+        if ((fingers + i) % (n+1) != 1) {
+            fingers2Show++;
+        }
+    }
+    printf("%d\n", fingers2Show); 
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

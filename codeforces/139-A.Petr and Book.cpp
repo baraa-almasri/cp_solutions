bd84354a69af442419f07fa2ef17cadc53a853fe
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-05-18 00:56:56 +0300

    hmm

---
+++ codeforces/139-A.Petr and Book.cpp
@@ -0,0 +1,66 @@
+// https://codeforces.com/problemset/problem/139/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    int n;
+    scanf("%d", &n);
+    int *days = new int[7];
+    for (int i{0}; i < 7; i++) {
+        scanf("%d", &days[i]);
+    }
+
+    for (int i{0}; ; i = (i+1)%7) {
+        n -= days[i];
+        if (n <= 0) {
+            printf("%d\n", i+1);
+            return;
+        }
+    }
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

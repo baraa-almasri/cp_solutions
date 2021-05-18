a6745b78edfe3cafb9581be0611c06e62e33afd9
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-05-18 23:44:38 +0300

    hmm

---
+++ codeforces/224-A.Parallelpiped.cpp
@@ -0,0 +1,60 @@
+// https://codeforces.com/problemset/problem/224/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    int a, a2, a3;
+    scanf("%d %d %d", &a, &a2, &a3);
+
+    printf("%.0f\n",
+        (4 * sqrt((a*a3)/a2)) + 
+        (4 * sqrt((a3*a2)/a)) + 
+        (4 * sqrt((a2*a)/a3))
+    );
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

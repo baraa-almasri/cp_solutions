09505b8a8b01bd17709c068bd73c53c1ea85506a
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-05-19 16:55:29 +0300

    hack goes brr...

---
+++ codeforces/199-A.Hexadecimal's theorem.cpp
@@ -0,0 +1,55 @@
+// https://codeforces.com/contest/199/problem/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    lli n;
+    scanf("%lld", &n);
+    printf("0 0 %lld\n", n);
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

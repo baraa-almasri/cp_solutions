aa757535fe10a37555e1cf050d4c05bda5386c86
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-05-20 06:09:12 +0300

    hmm

---
+++ codeforces/43-A.Football.cpp
@@ -0,0 +1,67 @@
+// https://codeforces.com/problemset/problem/43/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    int n;
+    scanf("%d", &n);
+    string team(10, '\0');
+    map<string, int> freqs;
+    string maxTeam;
+    int max{INT32_MIN};
+    while (n--) {
+        cin >> team;
+        freqs[team]++;
+        if (max < freqs[team]) {
+            maxTeam = team;
+            max = freqs[team];
+        }
+    }
+    cout << maxTeam << '\n';    
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

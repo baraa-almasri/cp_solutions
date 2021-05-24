f5f90c9a3ed4e4b2eac13dc7e31351204752bd95
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-05-25 01:17:01 +0300

    hot from round 722-div.2

---
+++ codeforces/1529-A.Eshag Loves Big Arrays.cpp
@@ -0,0 +1,63 @@
+// https://codeforces.com/problemset/problem/1529/A
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <new>
+#include <functional>
+#include <set>
+#include <queue>
+#include <stack>
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+using namespace std;
+
+//#define DEBUG_BLYAT
+
+/*
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+ */
+
+void solve() {
+    int n;
+    scanf("%d", &n);
+
+    map<int, int> freqs;
+    int tmp, min{INT32_MAX};
+    while (n--) {
+        scanf("%d", &tmp);
+        freqs[tmp]++;
+        min = (tmp < min? tmp: min);
+    }
+    printf("%d\n", n-freqs[min]);
+}
+
+int main() {
+    #ifdef DEBUG_BLYAT
+    freopen("../gulag/input.txt", "r", stdin);
+    #endif
+
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

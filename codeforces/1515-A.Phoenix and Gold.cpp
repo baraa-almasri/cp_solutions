4145d4e6b70f9b86e3bbff66643ce12170e28e5b
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-07-04 14:40:46 +0300

    hmm

---
+++ codeforces/1515-A.Phoenix and Gold.cpp
@@ -0,0 +1,90 @@
+// https://codeforces.com/contest/1515
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+#define watch(x) cout << #x <<" = " << x << '\n';
+
+static lli n;
+
+template <typename type>
+inline std::ostream& operator<<(std::ostream& os, std::vector<type> &vec){
+    os << "[";
+    for (int i = 0; i < vec.size(); i++) { 
+        os << vec[i] << (i != vec.size() - 1? ", ": ""); 
+    }
+    os << "]\n";
+    return os;
+}
+
+template <typename type>
+inline std::istream& operator>>(std::istream& is, std::vector<type> &vec){
+    for (int i = 0; i < vec.size(); i++) {
+        is >> vec[i]; 
+    }
+    return is;
+}
+
+template <typename type>
+inline std::istream& operator>>(std::istream& is, type *arr){
+    for (int i = 0; i < n; i++) {
+        is >> arr[i]; 
+    }
+    return is;
+}
+
+inline bool charExist(char chr, std::string str) {  
+    return str.find(chr) != std::string::npos;
+}
+
+/*
+ * [[[[[[[[[[[[[[[[hello]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[darkness]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[my]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[old]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[friend]]]]]]]]]]]]]]]]
+ */
+
+inline void solve() {
+    int x;
+    scanf("%lld %d", &n, &x);
+    vector<int> w(n);
+    cin >> w;
+
+    if (accumulate(all(w), 0) == x) {
+        puts("NO");
+        return;
+    } 
+
+    puts("YES");
+
+    int currSum{0};
+    for (int i{0}; i < w.size(); i++) {
+        currSum += w[i];
+        if (currSum == x) {
+            currSum -= w[i];
+            w.push_back(w[i]);
+            continue;
+        }
+        printf("%d ", w[i]);
+    }
+    
+    puts("");
+}
+
+int main() {
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

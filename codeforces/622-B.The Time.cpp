8784135dfc09f763d70596e994002ac469d8bc9a
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-07-05 19:27:22 +0300

    oooweee

---
+++ codeforces/622-B.The Time.cpp
@@ -0,0 +1,76 @@
+// https://codeforces.com/contest/622/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+#define watch(x) cout << #x <<" = " << x << '\n';
+
+static lli n;
+
+template <typename type> inline std::ostream& operator<<(std::ostream& os, std::vector<type> &vec){
+    os << "[";
+    for (int i = 0; i < vec.size(); i++) { 
+        os << vec[i] << (i != vec.size() - 1? ", ": ""); 
+    }
+    os << "]\n";
+    return os;
+}
+
+template <typename type> inline std::istream& operator>>(std::istream& is, std::vector<type> &vec){
+    for (int i = 0; i < vec.size(); i++) {
+        is >> vec[i]; 
+    }
+    return is;
+}
+
+template <typename type> inline std::istream& operator>>(std::istream& is, type *arr){
+    for (int i = 0; i < n; i++) {
+        is >> arr[i]; 
+    }
+    return is;
+}
+
+inline bool charExist(char chr, std::string str) {  
+    return str.find(chr) != std::string::npos;
+}
+
+/*
+ * [[[[[[[[[[[[[[[[hello]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[darkness]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[my]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[old]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[friend]]]]]]]]]]]]]]]]
+ */
+
+inline string formatNumber(int n) {
+    return n >= 10? to_string(n): "0" + to_string(n);
+}
+
+inline void solve() {
+    int h, m;
+    scanf("%d:%d", &h, &m);
+    int a;
+    scanf("%d", &a);
+
+    h = (h+((m + a)/60)) % 24;
+    m = (m + a) % 60;
+
+    cout << formatNumber(h) << ':' << formatNumber(m) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(true);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    // scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

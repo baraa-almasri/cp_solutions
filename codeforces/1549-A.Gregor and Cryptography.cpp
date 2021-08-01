b112d8a2cbcc4b71fcc506bf3748cda3f38263f7
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-08-01 22:56:25 +0300

    ..

---
+++ codeforces/1549-A.Gregor and Cryptography.cpp
@@ -0,0 +1,74 @@
+// https://codeforces.com/contest/1549/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long lli;
+typedef unsigned long long llu;
+#define all(v) v.begin(), v.end()
+#define allArr(a,n) a, a+n
+#define watch(x) cout << #x <<" = " << x << '\n';
+
+static lli n;
+
+template <typename type> inline std::ostream& operator<<(std::ostream& os, std::vector<type> &vec){
+    os << "[";
+    for (int i = 0; i < vec.size(); i++) { 
+        os << vec[i] << (i != vec.size() - 1? ", ": ""); 
+    }
+    os << "]\n";
+    return os;
+}
+
+template <typename type> inline std::istream& operator>>(std::istream& is, std::vector<type> &vec){
+    for (int i = 0; i < vec.size(); i++) {
+        is >> vec[i]; 
+    }
+    return is;
+}
+
+template <typename type> inline std::istream& operator>>(std::istream& is, type *arr){
+    for (int i = 0; i < n; i++) {
+        is >> arr[i]; 
+    }
+    return is;
+}
+
+inline bool charExist(char chr, std::string str) {  
+    return str.find(chr) != std::string::npos;
+}
+
+/*
+ * [[[[[[[[[[[[[[[[hello]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[darkness]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[my]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[old]]]]]]]]]]]]]]]]
+ * [[[[[[[[[[[[[[[[friend]]]]]]]]]]]]]]]]
+ */
+
+inline void solve() {
+    lli p;
+    scanf("%lld", &p);
+
+    for (lli a{2}; a*a <= p; a++) {
+        for (lli b{p}; b*b >= p; b--) {
+            if (p%a == p%b) {
+                printf("%lld %lld\n", a, b);
+                return;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t{1};
+    scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
